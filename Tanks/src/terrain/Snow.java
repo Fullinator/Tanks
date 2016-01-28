eb0f6d92171f0fa29aa0ddb31bab9e72efe27528
Joel Cherney <jjcherne@mtu.edu>
2016-01-28 10:08:07 -0500

    Created Terrain Classes

---
+++ Tanks/src/terrain/Snow.java
@@ -0,0 +1,45 @@
+package terrain;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import drawable.drawable;
+
+public class Snow extends Terrain {
+	
+	public Snow(int x, int y) {
+		super(x, y, y);
+	}
+
+	@Override
+	protected void fill() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDrawable(ArrayList<drawable> drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int findY(int x) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void generate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ Tanks/src/terrain/Terrain.java
@@ -0,0 +1,152 @@
+package terrain;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import drawable.drawable;
+
+
+@SuppressWarnings("serial")
+public abstract class Terrain extends JPanel{
+	protected boolean[][] terrain; // This will hold all of the points that will be painted
+	protected int xPanel = 0;// This will be set to the JPanels width
+	protected int yPanel = 0;// This will be set to the JPanels height
+	public int maxPlayers = -1;
+	public int currentPlayer = 1;
+	protected double y;
+	protected double a;
+	protected double b;
+	protected double c;
+	protected double d;
+	protected int maxHuman = -1;
+	public JLabel angle;
+	public JLabel power;
+    protected ArrayList<drawable> drawable;
+	
+	/**
+	 *
+	 * @param x width of JPanel
+	 * @param y height of JPanel
+	 */
+	protected Terrain(int x, int y, int maxH) {
+		setXTerrain(x);
+		setYTerrain(y);
+		maxHuman = maxH;
+		generate();
+	}
+	
+	public drawable currentTank() {
+		for ( int i = 0; i < drawable.size(); i++) {
+			if (drawable.get(i).playerNumber() == currentPlayer) {
+				return drawable.get(i);
+			}
+		}
+		return null;
+	}
+	
+
+	/**
+	 * used to get the array of points used to draw the terrain
+	 * 
+	 * @return boolean array of points
+	 */
+	public boolean[][] getTerrain(){
+		return terrain;
+	}
+
+	public abstract void setDrawable(ArrayList<drawable> drawable);
+
+	public abstract int findY(int x); 
+
+	/**
+	 *
+	 * @param x the JPanels width
+	 */
+	public void setXTerrain(int x) {
+		xPanel = x;
+	}
+
+	/**
+	 *
+	 * @param y the JPanels height
+	 */
+	public void setYTerrain(int y) {
+		yPanel = y;
+	}
+
+	/**
+	 *
+	 * @return the JPanels width
+	 */
+	public int getXTerrain() {
+		return xPanel;
+	}
+
+	/**
+	 *
+	 * @return the JPanels height
+	 */
+	public int getYTerrain() {
+		return yPanel;
+	}
+
+
+	/**
+	 * Generate random points and uses cubic regression to find the equation of the line
+	 */
+	protected abstract void generate();
+
+	/**
+	 * fills the space underneath the cubic
+	 */
+	protected abstract void fill();
+
+
+	/**
+	 * Removes a circle from the boolean terrain array
+	 *
+	 * @param x X coordinate of the center of the hole
+	 * @param y Y coordinate of the center of the hole
+	 * @param mag magnitude of the hole 
+	 *
+	 */
+	public void damage(int x, int y, int mag){
+		//create the hole
+		for(int i = y + mag; i > y - mag; i -= 1){
+			int low = (int)(-Math.sqrt(Math.pow(mag, 2) - Math.pow(i - y, 2)) + x);// Finds the lower x coordinate for the given y coordinate
+			int high = (int)(Math.sqrt(Math.pow(mag, 2) - Math.pow(i - y, 2)) + x);// Finds the upper x coordiante for the given y corrdinate
+			for(int j = low; j < high; j += 1){// loops from the lower x to the upper x
+				if(j >= 0 && j < xPanel && i >= 0 && i < yPanel){
+					terrain[j][i] = false;//sets points equal to false
+				}
+			}
+		}
+
+		//implement gravity
+		for(int k = 0; k < yPanel; k += 1){
+			for(int i = x - mag; i < x + mag; i += 1){
+				for(int j = y + mag; j > 0; j -= 1){
+					if(j + 1 < yPanel && j > 0 && i > 0 && i < xPanel){
+						if (terrain[i][j] && !terrain[i][j + 1]){
+							terrain[i][j+1] = true;
+							terrain[i][j] = false;
+							repaint();
+						}
+					}
+				}
+			}
+		}
+		repaint();
+	}//end of the remove method
+
+	/**
+	 * Calls the super paintComponent to paint on the JPanel
+	 */
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+	}//end of paintComponent method
+
+}// End of abstract Terrain Class
