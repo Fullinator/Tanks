8e666c4422712d6cbd63d22781181a6c0f8e52cb
Joel Cherney <jjcherne@mtu.edu>
2016-02-26 11:07:40 -0500

    updated the rate the sun and moon move updated the rate the the day and night cycle

--- Tanks/src/drawable/DayCycle.java
+++ Tanks/src/drawable/DayCycle.java
@@ -9,15 +9,17 @@ import javax.imageio.ImageIO;
 
 public class DayCycle implements Drawable2 {
 	private BufferedImage sun;
+	private BufferedImage moon;
 	private int x = -150;
 	private int y = 0;
 	private int radius;
-	private int wait = 0;
+	private int wait = 3;
 	private int count = 0;
 	private int yLength;
 	private int xLength;
 	private int xEquation = (int) Math.sqrt(Math.pow(y, 2) - radius);
 	private int yEquation;
+	private boolean day = true;
 
 	public DayCycle(int xLength, int yLength) {
 		try {                
@@ -25,9 +27,14 @@ public class DayCycle implements Drawable2 {
 		} catch (IOException ex) {
 			System.out.println("The sun file requested does not exist! Please fix this before contueing!");
 		}
+		try {                
+			moon = ImageIO.read(getClass().getResourceAsStream("/img/moon.png"));
+		} catch (IOException ex) {
+			System.out.println("The moon file requested does not exist! Please fix this before contueing!");
+		}
 		this.yLength = yLength;
 		this.xLength = xLength;
-		
+
 		radius = xLength/2;
 		yEquation  = (int) Math.sqrt(Math.pow((x - xLength/2), 2) - radius);
 	}
@@ -46,6 +53,10 @@ public class DayCycle implements Drawable2 {
 
 	@Override
 	public int getX() {
+		if (x == (xLength + 5)) {
+			x = -150;
+			day = !day;
+		}
 		if ( count == wait) {
 			x++;
 			count = 0;
@@ -59,12 +70,16 @@ public class DayCycle implements Drawable2 {
 	@Override
 	public int getY() { 
 		//System.out.println("x: " + x + "       Y: " + (int) (850 - (Math.sqrt(Math.abs(-Math.pow(x, 2)+800*x+200000)))));
-		return (int) (850 - (Math.sqrt(Math.abs(-Math.pow(x, 2)+800*x+200000))));
+		return (int) (800 - (Math.sqrt(Math.abs(-Math.pow(x, 2)+800*x+200000))));
 	}
 
 	@Override
 	public BufferedImage queryImage() {
-		return sun;
+		if (day) {
+			return sun;
+		}else {
+			return moon;
+		}
 	}
 
 }
